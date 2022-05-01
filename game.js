414f88ebffe0b174684e45a50f52d1392905792e
unknown <hk32077@gmail.com>
2022-04-30 21:13:22 -0500

    Added questions to an array in the game.js file

---
+++ game.js
@@ -0,0 +1,89 @@
+function triviaStart() {
+    console.log("Test")
+}
+
+let questions = [
+    {
+        question: `Who is the winningest team in NFL history?`,
+        choice1: `Chicago Bears`,
+        choice2: `Green Bay Packers`,
+        choice3: `Atlanta Falcons`,
+        choice4: `Kansas City Chiefs`,
+        answer: 2,
+    },
+    {
+        question: `Who is the all time passing yards leader in NFL history?`,
+        choice1: `Peyton Manning`,
+        choice2: `Aaron Rodgers`,
+        choice3: `Joe Montana`,
+        choice4: `Tom Brady`,
+        answer: 4,
+    },
+    {
+        question: `Where did Aaron Rodgers attend college?`,
+        choice1: `Alabama`,
+        choice2: `Clemson`,
+        choice3: `UC Berkley`,
+        choice4: `Michigan`,
+        answer: 3,
+    },
+    {
+        question: `Who is the all time receiving yards leader in NFL history?`,
+        choice1: `Jerry Rice`,
+        choice2: `Calvin Johnson`,
+        choice3: `Terrell Owens`,
+        choice4: `Jordy Nelson`,
+        answer: 1,
+    },
+    {
+        question: `Which team won the first Super Bowl ever?`,
+        choice1: `New York Giants`,
+        choice2: `Cincinnati Bengals`,
+        choice3: `Green Bay Packers`,
+        choice4: `Buffalo Bills`,
+        answer: 3,
+    },
+    {
+        question: `Who is the winningest head coach in NFL history?`,
+        choice1: `Bill Belichick`,
+        choice2: `Don Shula`,
+        choice3: `Vince Lombardi`,
+        choice4: `Mike McCarthy`,
+        answer: 2,
+    },
+    {
+        question: `Dan Marino played quarterback for which NFL team?`,
+        choice1: `Miami Dolphins`,
+        choice2: `Minnesota Vikings`,
+        choice3: `Detroit Lions`,
+        choice4: `New York Jets`,
+        answer: 1,
+    },
+    {
+        question: `How many Super Bowls have the Green Bay Packers won?`,
+        choice1: `3`,
+        choice2: `5`,
+        choice3: `4`,
+        choice4: `2`,
+        answer: 3,
+    },
+    {
+        question: `Who won Super Bowl LVI?`,
+        choice1: `Los Angeles Rams`,
+        choice2: `Kansas City Chiefs`,
+        choice3: `New York Giants`,
+        choice4: `Tampa Bay Buccaneers`,
+        answer: 1,
+    },
+    {
+        question: `Which team holds the record for the most Super Bowl appearances?`,
+        choice1: `Green Bay Packers`,
+        choice2: `Buffalo Bills`,
+        choice3: `New Orleans Saints`,
+        choice4: `New England Patriots`,
+        answer: 4,
+    },
+]
+
+
+playGame()
